35bc68b6cd08b631b468d413261969a18a9e0de4
5theldritchabomination <thibault.royer1@ecoles-epsi.net>
2025-12-02 15:16:50 +0100

    TP4 : ajout film image

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,60 @@
 import moviesList from './data/moviesList.js';
-import './style.css';
-import styles from './App.module.css';
 export default function App() {
   const moviesListf = moviesList();
   console.log(moviesListf);
   return (
     <>
-      <h1 className={styles.title}>CinéTech</h1>
-      <main>
-        <h2>Films à l'affiche</h2>
-        <ul>
+      <h1 className="text-4xl font-bold text-center my-6">CinéTech</h1>
+      <main className="max-w-7xl mx-auto mt-6">
+        <h2 className="text-2xl font-bold mb-4">Films à l'affiche</h2>
+        <ul className="grid grid-cols-1 sm:grid-cols-5 gap-6">
           {moviesListf.map((movie) => (
-            <li key={movie.id}>
+            <li key={movie.id} className="bg-gray-100 rounded-xl p-4 shadow">
               <h3>{movie.title}</h3>
               <p>
                 {movie.year} — {movie.director}
               </p>
               <p>Note : {movie.rating}</p>
+              <img
+                src={movie.poster}
+                className="w-full h-64 object-cover rounded"
+              ></img>
             </li>
           ))}
         </ul>
-        <h2>Films coups de coeur</h2>
-        <ul>
+        <h2 className="text-2xl font-bold mb-4">Films coups de coeur</h2>
+        <ul className="grid grid-cols-1 sm:grid-cols-5 gap-6">
           {moviesListf
             .filter((movie) => movie.rating >= 8)
             .map((movie) => (
-              <li key={movie.id}>
+              <li key={movie.id} className="bg-gray-100 rounded-xl p-4 shadow">
                 <h3>{movie.title}</h3>
                 <p>
                   {movie.year} — {movie.director}
                 </p>
                 <p>Note : {movie.rating}</p>
+                <img
+                  src={movie.poster}
+                  className="w-full h-64 object-cover rounded"
+                ></img>
               </li>
             ))}
         </ul>
-        <h2>Films de super hero</h2>
-        <ul>
+        <h2 className="text-2xl font-bold mb-4">Films de super hero</h2>
+        <ul className="grid grid-cols-1 sm:grid-cols-5 gap-6">
           {moviesListf
             .filter((movie) => movie.genre.includes('super-hero'))
             .map((movie) => (
-              <li key={movie.id}>
+              <li key={movie.id} className="bg-gray-100 rounded-xl p-4 shadow">
                 <h3>{movie.title}</h3>
                 <p>
                   {movie.year} — {movie.director}
                 </p>
                 <p>Note : {movie.rating}</p>
+                <img
+                  src={movie.poster}
+                  className="w-full h-64 object-cover rounded"
+                ></img>
               </li>
             ))}
         </ul>
