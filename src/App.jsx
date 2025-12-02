fa8e7b9e89f89d367f57adaa68de8155eda1d39f
5theldritchabomination <thibault.royer1@ecoles-epsi.net>
2025-12-02 14:34:40 +0100

    TP3 : films coups de cœur

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,20 @@ export default function App() {
     <>
       <h1>CinéTech</h1>
       <main>
+        <h2>Films coups de coeur</h2>
+        <ul>
+          {moviesListf
+            .filter((movie) => movie.rating >= 8)
+            .map((movie) => (
+              <li key={movie.id}>
+                <h3>{movie.title}</h3>
+                <p>
+                  {movie.year} — {movie.director}
+                </p>
+                <p>Note : {movie.rating}</p>
+              </li>
+            ))}
+        </ul>
         <h2>Films à l'affiche</h2>
         <ul>
           {moviesListf.map((movie) => (
