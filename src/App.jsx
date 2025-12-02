cf325adc0d1df40fc7dc6132eba88d0734ad7cce
5theldritchabomination <thibault.royer1@ecoles-epsi.net>
2025-12-02 14:28:02 +0100

    TP3 : liste des films

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,24 @@
+import moviesList from './data/moviesList.js';
 export default function App() {
-  return <h1>CinéTech</h1>;
+  const moviesListf = moviesList();
+  console.log(moviesListf);
+  return (
+    <>
+      <h1>CinéTech</h1>
+      <main>
+        <h2>Films à l'affiche</h2>
+        <ul>
+          {moviesListf.map((movie) => (
+            <li key={movie.id}>
+              <h3>{movie.title}</h3>
+              <p>
+                {movie.year} — {movie.director}
+              </p>
+              <p>Note : {movie.rating}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </>
+  );
 }
