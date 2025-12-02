a4affaa9278b10078970f3332c7553d3b752515b
5theldritchabomination <thibault.royer1@ecoles-epsi.net>
2025-12-02 14:48:16 +0100

    TP4 : style css classique

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import moviesList from './data/moviesList.js';
+import './style.css';
 export default function App() {
   const moviesListf = moviesList();
   console.log(moviesListf);
   return (
     <>
-      <h1 style={{ color: 'red', backgroundColor: 'yellow' }}>CinéTech</h1>
+      <h1 className="title">CinéTech</h1>
       <main>
-        <h2 style={{ fontSize: '20px' }}>Films à l'affiche</h2>
+        <h2>Films à l'affiche</h2>
         <ul>
           {moviesListf.map((movie) => (
             <li key={movie.id}>
@@ -18,7 +19,7 @@ export default function App() {
             </li>
           ))}
         </ul>
-        <h2 style={{ fontSize: '20px' }}>Films coups de coeur</h2>
+        <h2>Films coups de coeur</h2>
         <ul>
           {moviesListf
             .filter((movie) => movie.rating >= 8)
@@ -32,7 +33,7 @@ export default function App() {
               </li>
             ))}
         </ul>
-        <h2 style={{ fontSize: '20px' }}>Films de super hero</h2>
+        <h2>Films de super hero</h2>
         <ul>
           {moviesListf
             .filter((movie) => movie.genre.includes('super-hero'))
